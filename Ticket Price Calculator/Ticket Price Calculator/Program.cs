67402cb72647a12623470c0d1c375478d9b525c1
SenaDAfari <150682177+SenaDAfari@users.noreply.github.com>
2025-08-05 09:38:29 +0000

    Ticket price calculator

---
+++ Ticket Price Calculator/Ticket Price Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your age: ");
+        string input = Console.ReadLine();
+
+        
+        if (int.TryParse(input, out int age))
+        {
+            int ticketPrice;
+
+            if (age <= 12 || age >= 65)
+                ticketPrice = 7; // Discounted price
+            else
+                ticketPrice = 10; // Regular price
+
+            Console.WriteLine($"Your ticket price is GHC{ticketPrice}.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid age.");
+        }
+    }
+}
